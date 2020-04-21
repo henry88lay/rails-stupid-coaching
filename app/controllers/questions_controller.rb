1f0e1b312a48b0021da62c4c1d4378a85b2bafa1
henry lay <henry88lay@gmail.com>
2020-04-21 20:23:00 +1000

    got it to MVP

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @answer = params[:answer]
+    if @answer.end_with? "?" || nil
+      @response = 'Silly question, get dress and go to work!'
+    elsif @answer == "I am going to work right now!"
+      @response = "Great!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
   end
-
 end
